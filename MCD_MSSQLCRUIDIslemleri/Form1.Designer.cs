c1c85f61cd136a15fecca1c3ea8ccc463ec37a39
ECR_EXCALIBUR_G770 <ECR_EXCALIBUR_G770@DESKTOP-TUMHS1A>
2021-12-20 21:20:43 +0300

    Add project files.

---
+++ MCD_MSSQLCRUIDIslemleri/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace MCD_MSSQLCRUIDIslemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnUrunGetir = new System.Windows.Forms.Button();
+            this.txtBulId = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtstok = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGetirId = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(856, 449);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 506);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(107, 506);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(211, 506);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnUrunGetir
+            // 
+            this.btnUrunGetir.Location = new System.Drawing.Point(321, 506);
+            this.btnUrunGetir.Name = "btnUrunGetir";
+            this.btnUrunGetir.Size = new System.Drawing.Size(117, 23);
+            this.btnUrunGetir.TabIndex = 4;
+            this.btnUrunGetir.Text = "Ürün Getir";
+            this.btnUrunGetir.UseVisualStyleBackColor = true;
+            this.btnUrunGetir.Click += new System.EventHandler(this.btnUrunGetir_Click);
+            // 
+            // txtBulId
+            // 
+            this.txtBulId.Location = new System.Drawing.Point(23, 608);
+            this.txtBulId.Name = "txtBulId";
+            this.txtBulId.Size = new System.Drawing.Size(100, 22);
+            this.txtBulId.TabIndex = 5;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(636, 506);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 22);
+            this.txtAd.TabIndex = 6;
+            // 
+            // txtstok
+            // 
+            this.txtstok.Location = new System.Drawing.Point(636, 562);
+            this.txtstok.Name = "txtstok";
+            this.txtstok.Size = new System.Drawing.Size(100, 22);
+            this.txtstok.TabIndex = 7;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(636, 632);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 22);
+            this.txtFiyat.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(508, 511);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "AD:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(508, 567);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "STOK:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(508, 637);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "FİYAT:";
+            // 
+            // btnGetirId
+            // 
+            this.btnGetirId.Location = new System.Drawing.Point(157, 596);
+            this.btnGetirId.Name = "btnGetirId";
+            this.btnGetirId.Size = new System.Drawing.Size(177, 47);
+            this.btnGetirId.TabIndex = 12;
+            this.btnGetirId.Text = "ID\' ye Göre Ürün Getir";
+            this.btnGetirId.UseVisualStyleBackColor = true;
+            this.btnGetirId.Click += new System.EventHandler(this.btnGetirId_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 677);
+            this.Controls.Add(this.btnGetirId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.txtstok);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtBulId);
+            this.Controls.Add(this.btnUrunGetir);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnUrunGetir;
+        private System.Windows.Forms.TextBox txtBulId;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtstok;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGetirId;
+    }
+}
+
